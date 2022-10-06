ae4cfb3582a9fbcd0e7bb92dbc82692686763de7
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-06 18:52:53 +0300

    Убрал лишние комментарии

--- EmailServiceWebApp/Service.cs
+++ EmailServiceWebApp/Service.cs
@@ -22,29 +22,23 @@ namespace EmailServiceWebApp
             //
             try
             {
-                var message = new MimeMessage(); // Класс библиотеки MailKit
-                message.From.Add(new MailboxAddress("EmailServiceWebApp", configEmailServer.EmailFrom));  //адрес отправления
-                message.To.Add(new MailboxAddress("NoName", configEmailServer.EmailTo)); //адрес назначения
-                message.Subject = configEmailServer.Subject;//тема сообщения
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("EmailServiceWebApp", configEmailServer.EmailFrom)); 
+                message.To.Add(new MailboxAddress("NoName", configEmailServer.EmailTo)); 
+                message.Subject = configEmailServer.Subject;
                 message.Body = new BodyBuilder()
                 {
-                    HtmlBody = configEmailServer.Body, // тело сообщения
+                    HtmlBody = configEmailServer.Body, 
                 }.ToMessageBody();
-                //message.Body = new TextPart("plain")
-                //{
-                //    Text = @"Дети хотят стать взрослыми. Взрослые хотят стать детьми. И только подростки уже устали от жизни и хотят сдохнуть"
-                //};
 
                 using (var client = new SmtpClient())
                 {
                     client.Connect("smtp.gmail.com", 587);
 
+                    // since we don't have an OAuth2 token, disable the OAUTH2 authentication mechanism.
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");  
 
-                    // Note: since we don't have an OAuth2 token, disable
-                    // the XOAUTH2 authentication mechanism.
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-                    // Note: only needed if the SMTP server requires authentication
+                    // only needed if the SMTP server requires authentication
                     client.Authenticate(configEmailServer.EmailFrom, configEmailServer.Password);
 
                     client.Send(message);
