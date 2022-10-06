31f988865e3ec146d43cae20796f8a26f33f6a8e
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-06 13:59:27 +0300

    Add project files.

---
+++ EmailServiceWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmailServiceWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
