f6963242e778224ef9becb545fc13168f5f684c5
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 19:46:25 +0300

    Создал миграцию инициализованной бд. При создании инициализации созданной бд не должно быть + должно быть Database.EnsureCreated(); (командой Add-Migration initialization -context DatabaseContext в Package Manager Console) Если не нужно менять бд то должно стоять Database.EnsureCreated(); При накатывании миграции нужно менять на Database.Migrate(); и прописывать новую миграцию в Package Manager Console

--- EmailService.Db/DatabaseContext.cs
+++ EmailService.Db/DatabaseContext.cs
@@ -1,16 +1,17 @@
-﻿using EmailService.Db.Models;
+﻿using System;
+using EmailService.Db.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmailService.Db
 {
     public class DatabaseContext: DbContext
     {
-        // Доступ к таблицам
         public DbSet<MailsItem> MailsItems { get; set; }
         public DbSet<Recipient> Recipients { get; set; }
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-            Database.Migrate();
+            Database.EnsureCreated();
+            //Database.Migrate();
         }
     }
 }
