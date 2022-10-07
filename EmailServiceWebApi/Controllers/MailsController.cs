23813373b9056c255bbaafa914e7cb15add9966e
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 10:05:49 +0300

    Добваил в контроллер методы GetAll и Send

--- EmailServiceWebApi/Controllers/MailsController.cs
+++ EmailServiceWebApi/Controllers/MailsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections;
+using System.Collections.Generic;
+using EmailServiceWebApi.Interfaces;
+using EmailServiceWebApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmailServiceWebApi.Controllers
@@ -7,5 +11,26 @@ namespace EmailServiceWebApi.Controllers
     [ApiController]
     public class MailsController : ControllerBase
     {
+        public IMailsRepository MailsItems { get; set; }
+        public MailsController(IMailsRepository mails)
+        {
+            MailsItems = mails;
+        }
+
+        public IEnumerable<MailsItem> GetAll()
+        {
+            return MailsItems.GetAll();
+        }
+
+        [HttpPost]
+        public IActionResult Send([FromBody] MailsItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            MailsItems.Add(item);
+            return CreatedAtRoute("GetMails", new { id = item.Key }, item);
+        }
     }
 }
