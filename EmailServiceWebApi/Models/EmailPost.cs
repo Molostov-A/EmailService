104bb7adfd712b8e58ab10c1b62076d63756a0c5
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 09:13:19 +0300

    добавил атрибуты

--- EmailServiceWebApi/Models/EmailPost.cs
+++ EmailServiceWebApi/Models/EmailPost.cs
@@ -1,9 +1,16 @@
-﻿namespace EmailServiceWebApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace EmailServiceWebApi.Models
 {
     public class EmailPost
     {
+        [JsonPropertyName("subject")]
         public string Subject { get; set; }
+
+        [JsonPropertyName("body")]
         public string Body { get; set; }
+
+        [JsonPropertyName("recipients")]
         public string[] Recipients { get; set; }
 
     }
