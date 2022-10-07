7e1db1786a2ff9ead6d394c4ceadef190051cc68
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 09:48:26 +0300

    Добавил пустой контроллер

---
+++ EmailServiceWebApi/Controllers/MailsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailServiceWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailsController : ControllerBase
+    {
+    }
+}
