0eb3d5f9d655948299b162bac72b75ac04d308a2
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 09:11:22 +0300

    Добавил класс для запросов EmailPost

---
+++ EmailServiceWebApi/Models/EmailPost.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailServiceWebApi.Models
+{
+    public class EmailPost
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string[] Recipients { get; set; }
+
+    }
+}
