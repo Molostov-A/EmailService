c780eba6205132f044921803295dbc5abf9c898a
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 12:44:38 +0300

    Скопировал MailsItem в Db проект

---
+++ EmailService.Db/Models/MailsItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmailService.Db.Models
+{
+    public class MailsItem
+    {
+        public  string Key { get; set; }
+        
+        public DateTime Date { get; set; }
+        
+        public string Subject { get; set; }
+        
+        public string Body { get; set; }
+        
+        public string[] Recipients { get; set; }
+        
+        public string Result { get; set; }
+        
+        public string FailedMessage { get; set; }
+
+    }
+}
