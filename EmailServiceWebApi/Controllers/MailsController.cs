732fe1015de4da04a4cff357c5adb381fcbef56e
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-07 17:52:02 +0300

    Перенёс маппинги в отдельный класс Mapping в папке Helpers

--- EmailServiceWebApi/Controllers/MailsController.cs
+++ EmailServiceWebApi/Controllers/MailsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using EmailService.Db.Interfaces;
 using EmailService.Db.Models;
+using EmailServiceWebApi.Helpers;
 using EmailServiceWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,33 +23,7 @@ namespace EmailServiceWebApi.Controllers
         public IEnumerable<MailsItemViewGet> GetAll()
         {
             var mails = _mails.GetAll();
-            var mailsView = ConvertToMailsItemViewGet(mails);
-            return mailsView;
-        }
-
-        private List<MailsItemViewGet> ConvertToMailsItemViewGet(IEnumerable<MailsItem> mails)
-        {
-            var mailsView = new List<MailsItemViewGet>();
-            foreach (var mail in _mails.GetAll())
-            {
-                var recipients = new List<string>();
-                foreach (var recipient in mail.Recipients)
-                {
-                    recipients.Add(recipient.Email);
-                }
-                mailsView.Add
-                (
-                    new MailsItemViewGet()
-                    {
-                        Body = mail.Body,
-                        Subject = mail.Subject,
-                        FailedMessage = mail.FailedMessage,
-                        Recipients = recipients,
-                        Result = mail.Result
-                    }
-                );
-            }
-
+            var mailsView = Mapping.ToListMailsItemViewGet(mails);
             return mailsView;
         }
 
@@ -60,7 +35,7 @@ namespace EmailServiceWebApi.Controllers
                 return BadRequest();
             }
 
-            var item = ConvertToMailsItem(itemViewsView);
+            var item = Mapping.ToMailsItem(itemViewsView);
             
             _emailSender.SendEmailMessage(item);
             _mails.Add(item);
@@ -68,26 +43,6 @@ namespace EmailServiceWebApi.Controllers
             return CreatedAtRoute("GetMails", new { id = item.Id }, item);
         }
 
-        private MailsItem ConvertToMailsItem(MailsItemViews itemViewsView)
-        {
-            MailsItem item = new MailsItem();
-            List<Recipient> recipients = new List<Recipient>();
-            for (int i=0; i<itemViewsView.Recipients.Count; i++)
-            {
-                recipients.Add(new Recipient()
-                {
-                    Id = Guid.NewGuid(),
-                    Email = itemViewsView.Recipients[i],
-                    MailsItem = item
-                });
-            }
-            item = new MailsItem()
-            {
-                Subject = itemViewsView.Subject,
-                Body = itemViewsView.Body,
-                Recipients = recipients
-            };
-            return item;
-        }
+        
     }
 }
